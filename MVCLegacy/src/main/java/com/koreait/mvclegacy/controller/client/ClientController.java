9e93abebbbb8adc0a0aba660dc34f5aa237410ce
zino1187 <zino1187@gmail.com>
2020-12-29 13:56:51 +0900

    c

---
+++ MVCLegacy/src/main/java/com/koreait/mvclegacy/controller/client/ClientController.java
@@ -0,0 +1,43 @@
+package com.koreait.mvclegacy.controller.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.koreait.mvclegacy.model.member.MemberService;
+
+/*
+ * 관리자기능과 관련된 모든 요청을 처리하는 컨트롤러
+ * */
+
+@Controller
+public class ClientController {
+	// All < DEBUG < INFO < WARN < ERROR < FATAL < OFF
+	private static final Logger logger=LoggerFactory.getLogger(ClientController.class);
+	
+	@Autowired
+	private MemberService memberService;
+	
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String test() {
+		logger.debug("test() 메서드 호출");
+		memberService.regist();
+		
+		return null;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
