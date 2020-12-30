fe07ebcbc3fbaaf9e83b28e69cad05eaab09681e
zino1187 <zino1187@gmail.com>
2020-12-30 10:28:21 +0900

    c

---
+++ MVCLegacy/src/main/java/com/koreait/mvclegacy/model/notice/JdbcNoticeDAO.java
@@ -0,0 +1,90 @@
+package com.koreait.mvclegacy.model.notice;
+/*
+ * 스프링이 지원하는 DB연동 기술에는 여러가지가 있다..
+ * [ Mybatis ]
+    SQL Mapper: 쿼리문과 자바객체간 매핑 
+    
+	[JDBC 자체]
+	
+    [ Hibernate ]
+	ORM (Object Relation Mapping) : 자바객체와 관계형DB와의 매핑
+	
+	[JPA]
+	.....
+ * */
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.koreait.mvclegacy.model.domain.Notice;
+
+@Repository
+public class JdbcNoticeDAO implements NoticeDAO{
+	private static final Logger logger=LoggerFactory.getLogger(JdbcNoticeDAO.class);
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate; //쿼리 수행객체
+	
+	//목록가져오기 
+	public List selectAll() {
+		List list = null;
+		String sql="select * from notice order by notice_id desc";
+		
+		//매개변수 순서: 쿼리문, 바인드변수를 배열로 처리, 매핑객체(ResultSet의 데이터를 담는객체)
+		list=jdbcTemplate.query(sql, new Object[]{}, new RowMapper<Notice>(){
+			public Notice mapRow(ResultSet rs, int rowNum) throws SQLException {
+				logger.debug("mapRow메서드 호출, rowNum ="+rowNum);
+				
+				Notice notice = new Notice();//create empty vo
+				notice.setNotice_id(rs.getInt("notice_id"));
+				notice.setTitle(rs.getString("title"));
+				notice.setWriter(rs.getString("writer"));
+				notice.setContent(rs.getString("content"));
+				notice.setRegdate(rs.getString("regdate"));
+				notice.setHit(rs.getInt("hit"));
+				
+				return notice;
+			}
+		});
+		return list;
+	}
+
+	@Override
+	public Notice select(int notice_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Notice notice) {
+		
+	}
+
+	@Override
+	public void update(Notice notice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int notice_id) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
+
+
+
+
+
+
--- MVCLegacy/src/main/java/com/koreait/mvclegacy/model/notice/NoticeService.java
+++ MVCLegacy/src/main/java/com/koreait/mvclegacy/model/notice/NoticeService.java
@@ -3,6 +3,7 @@ package com.koreait.mvclegacy.model.notice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.koreait.mvclegacy.exception.DMLException;
@@ -10,8 +11,10 @@ import com.koreait.mvclegacy.model.domain.Notice;
 
 @Service
 public class NoticeService {
-	@Autowired
-	private NoticeDAO noticeDAO;
+	//주입시키려 하는 자료형이 2개 이상을 경우, 개발자는 무엇을 사용할지 원하는 객체를 명시해야 한다..
+	@Autowired 
+	@Qualifier("jdbcNoticeDAO")
+	private NoticeDAO noticeDAO; //DI로 주입받기 위해서 상위 객체를 보유한다
 	
 	//CRUD method 
 	public List selectAll() {
