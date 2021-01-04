c8ac75dd36a085fa92f23c8b5b7164e845bd7661
zino1187 <zino1187@gmail.com>
2021-01-04 16:07:21 +0900

    c

--- FashionShop/src/main/java/com/koreait/fashionshop/model/product/service/ProductServiceImpl.java
+++ FashionShop/src/main/java/com/koreait/fashionshop/model/product/service/ProductServiceImpl.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.koreait.fashionshop.common.FileManager;
+import com.koreait.fashionshop.model.domain.Image;
 import com.koreait.fashionshop.model.domain.Product;
+import com.koreait.fashionshop.model.product.repository.ImageDAO;
 import com.koreait.fashionshop.model.product.repository.ProductDAO;
 
 @Service
@@ -15,7 +18,9 @@ public class ProductServiceImpl implements ProductService{
 	@Autowired
 	private ProductDAO productDAO;
 	
-
+	@Autowired
+	private ImageDAO imageDAO;
+	
 	@Override
 	public List selectAll() {
 		// TODO Auto-generated method stub
@@ -36,13 +41,35 @@ public class ProductServiceImpl implements ProductService{
 
 	@Override
 	public void regist(FileManager fileManager, Product product) {
+		
+		String ext=fileManager.getExtend(product.getRepImg().getOriginalFilename());
+		product.setFilename(ext); //확장자 결정
 		//db에 넣는 일은 DAO에게 시키고
 		productDAO.insert(product);
 		
 		//파일 업로드!!는 FileManager에게 시킨다
 		//대표이미지 업로드 
-		String basicImg = product.getProduct_id()+"."+fileManager.getExtend(product.getRepImg().getOriginalFilename());
+		String basicImg = product.getProduct_id()+"."+ext;
 		fileManager.saveFile(fileManager.getSaveBasicDir()+File.separator+basicImg, product.getRepImg());
+		
+		//추가이미지 업로드 (FileManager에게 추가이미지 갯수만큼 업로드 업무를 시키면 된다!!)
+		for(int i=0;i<product.getAddImg().length;i++) {
+			
+			MultipartFile file = product.getAddImg()[i];
+			ext = fileManager.getExtend(file.getOriginalFilename());
+			
+			//image table에 넣기!!
+			Image image = new Image();
+			image.setProduct_id(product.getProduct_id()); //fk
+			image.setFilename(ext); //확장자 넣기
+			imageDAO.insert(image);
+			
+			//메니져의 저장 메서드 호출
+			String addImg = image.getImage_id()+"."+ext;
+			fileManager.saveFile(fileManager.getSaveAddonDir()+File.separator+addImg, file);
+		}
+		
+		
 	}
 
 	@Override
