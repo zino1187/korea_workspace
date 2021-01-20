a583351a355640a452ca455812e73f8200091d12
zino1187 <zino1187@gmail.com>
2021-01-20 14:54:39 +0900

    c

--- RestProject/src/main/java/com/koreait/restproject/rest/controller/member/MemberController.java
+++ RestProject/src/main/java/com/koreait/restproject/rest/controller/member/MemberController.java
@@ -1,7 +1,11 @@
 package com.koreait.restproject.rest.controller.member;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.koreait.restproject.model.domain.Member;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,11 +13,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MemberController {
 	
+
 	@GetMapping("/member")
 	public String getList() {
 		log.debug("리스트 요청했어?");
 		return "ha ha ha";
 	}
+	
+	@PostMapping("/member")
+	public String regist(@RequestBody Member member) {
+		log.debug("등록을 원해?");
+		log.debug("m_id"+member.getM_id());
+		log.debug("m_pass"+member.getM_pass());
+		log.debug("m_name"+member.getM_name());
+		
+		return "regist success";
+	}
+	
 }
 
 
